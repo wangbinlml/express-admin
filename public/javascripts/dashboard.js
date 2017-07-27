81fa062b03f43ac735d1705bc93636159bad220d
wangbin <wangbinlml@gmail.com>
2017-07-28 00:29:56 +0800

    main page

---
+++ public/javascripts/dashboard.js
@@ -0,0 +1,43 @@
+/*
+ * Author: Abdullah A Almsaeed
+ * Date: 4 Jan 2014
+ * Description:
+ *      This is a demo file used only for the main dashboard (index.html)
+ **/
+
+$(function () {
+
+  'use strict';
+
+  // Make the dashboard widgets sortable Using jquery UI
+  $('.connectedSortable').sortable({
+    placeholder         : 'sort-highlight',
+    connectWith         : '.connectedSortable',
+    handle              : '.box-header, .nav-tabs',
+    forcePlaceholderSize: true,
+    zIndex              : 999999
+  });
+  $('.connectedSortable .box-header, .connectedSortable .nav-tabs-custom').css('cursor', 'move');
+
+  // jQuery UI sortable for the todo list
+  $('.todo-list').sortable({
+    placeholder         : 'sort-highlight',
+    handle              : '.handle',
+    forcePlaceholderSize: true,
+    zIndex              : 999999
+  });
+
+  // The Calender
+  $('#calendar').datepicker();
+
+  /* The todo list plugin */
+  $('.todo-list').todoList({
+    onCheck  : function () {
+      window.console.log($(this), 'The element has been checked');
+    },
+    onUnCheck: function () {
+      window.console.log($(this), 'The element has been unchecked');
+    }
+  });
+
+});
