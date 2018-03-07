87f0a12983d7b1d8965b8541deb711bb698358f2
wangbin <wangbin@hxkid.com>
2018-03-07 18:48:19 +0800

    get user's (client's) real ip-address

--- core/common.js
+++ core/common.js
@@ -1,8 +1,10 @@
 const stringUtils = require("./util/StringUtils");
 const mysql = require('./mysql');
+const log = require('../core/logger').getLogger("system");
 module.exports.saveLoginLog = async(req) => {
     var user = req.session.user;
     var ip = stringUtils.getReqRemoteIp(req);
+    log.info("客户端用户IP：",ip);
     await mysql.query("insert into bs_login_log(user_id,user_name,name,ip,login_time) values(?,?,?,?,?)", [user.id, user.user_name, user.name, ip, new Date()]);
 };
 module.exports.saveOperateLog = async(req, operations) => {
