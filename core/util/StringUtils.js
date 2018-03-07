fd7b11b2c127b9eab226e29da44ba70ac0d360c4
wang <wangbinlml@gmail.com>
2018-03-07 17:20:48 +0800

    获取ip
    获取ip

--- core/util/StringUtils.js
+++ core/util/StringUtils.js
@@ -71,6 +71,7 @@ module.exports.md5 = function (text) {
     return crypto.createHash('md5').update(text).digest('hex');
 };
 module.exports.getReqRemoteIp = function (req) {
-    return (req.headers['x-forwarded-for'] || '').split(',')[0] || req.ip;
+    var ip = req.headers["x-real-ip"] || req.ip;
+    return ip;
 }
 ;
